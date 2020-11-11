e41409746fc269dee646470723571c12c60b2b90
Edward Wright <edwardharrywright@gmail.com>
2020-11-11 15:25:25 +0000

    added URL generator

--- src/Components/Footer/Footer.svelte
+++ src/Components/Footer/Footer.svelte
@@ -1,9 +1,17 @@
+<script>
+  import URL from "./URL.svelte";
+</script>
+
 <style>
   .footer {
     margin: 0;
     padding: 40px 0 0 0;
     font-size: 0.7rem;
     color: var(--theme-text);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
   }
 
   a {
@@ -13,14 +21,18 @@
   @media (min-width: 980px) {
     .footer {
       padding: 70px 0 0 0;
+      flex-direction: row;
+      align-items: center;
     }
   }
 </style>
 
 <div class="footer">
-  <p>
-    Data provided by <a
+  <div>
+    Data provided by
+    <a
       href="https://www.football-data.org/"
       target="_blank">football-data.org</a>.
-  </p>
+  </div>
+  <URL />
 </div>
